15cb8f064dd3c438adb972e6bbc680db8b2e2eb8
MUsoftware <musoftware@daum.net>
2025-05-18 15:59:05 +0900

    chore: 모델들에 created_at / updated_at / deleted_at 및 공통 QuertSet 추가

--- app/cms/models.py
+++ app/cms/models.py
@@ -1,44 +1,59 @@
-import uuid
+import datetime
+import typing
 
+from core.models import BaseAbstractModel, BaseAbstractModelQuerySet
 from django.core.validators import MinValueValidator
 from django.db import models
 
 
-class Page(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    is_active = models.BooleanField(default=False)
+class Page(BaseAbstractModel):
     css = models.TextField(null=True, blank=True, default=None)
     title = models.CharField(max_length=256)
     subtitle = models.CharField(max_length=512)
-    # history = HistoricalRecords()
 
     def __str__(self):
         return str(self.title)
 
 
-class Sitemap(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class SitemapQuerySet(BaseAbstractModelQuerySet):
+    def filter_by_today(self) -> typing.Self:
+        today = datetime.date.today()
+        return self.filter(
+            models.Q(display_start_at__isnull=True) | models.Q(display_start_at__lte=today),
+            models.Q(display_end_at__isnull=True) | models.Q(display_end_at__gte=today),
+        )
+
+
+class Sitemap(BaseAbstractModel):
     parent_sitemap = models.ForeignKey(
-        "self", null=True, blank=True, default=None, on_delete=models.SET_NULL, related_name="children"
+        "self", null=True, default=None, on_delete=models.SET_NULL, related_name="children"
     )
+
     name = models.CharField(max_length=256)
     order = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     page = models.ForeignKey(Page, on_delete=models.PROTECT)
+
     display_start_at = models.DateTimeField(null=True, blank=True)
     display_end_at = models.DateTimeField(null=True, blank=True)
-    # history = HistoricalRecords()
+
+    objects: SitemapQuerySet = SitemapQuerySet.as_manager()
+
+    class Meta:
+        ordering = ["order"]
 
     def __str__(self):
         return str(self.name)
 
 
-class Section(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+class Section(BaseAbstractModel):
+    page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name="sections")
     order = models.IntegerField(default=0)
+
     css = models.TextField(null=True, blank=True, default=None)
     body = models.TextField(help_text="Content of the page, Written in markdown format")
-    # history = HistoricalRecords()
+
+    class Meta:
+        ordering = ["order"]
 
     def __str__(self):
         return f"Section {self.order} of {self.page}"
