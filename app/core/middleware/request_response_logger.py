ec76d6ffff6129c0f14c3ee4fae9a6a7ef955d0f
MUsoftware <musoftware@daum.net>
2025-06-01 04:17:06 +0900

    fix: 불필요한 constance 조회 제거

--- app/core/middleware/request_response_logger.py
+++ app/core/middleware/request_response_logger.py
@@ -2,7 +2,6 @@ import http.client
 import logging
 import typing
 
-from constance import config
 from core.logger.util.django_helper import (
     get_aws_request_id_from_request,
     get_request_log_data,
@@ -23,15 +22,9 @@ class LoggerExtraDataType(typing.TypedDict):
     tag: typing.NotRequired[str]
 
 
-class LoggerExtraSessionType(typing.TypedDict):
-    before: dict[str, typing.Any]
-    after: dict[str, typing.Any]
-
-
 class LoggerExtraType(typing.TypedDict):
     aws_request_id: str
     data: LoggerExtraDataType
-    session: typing.NotRequired[LoggerExtraSessionType]
 
 
 class RequestResponseLogger(MiddlewareMixin):
@@ -40,16 +33,12 @@ class RequestResponseLogger(MiddlewareMixin):
     get_response: GetResponseCallable
 
     def __call__(self, request: HttpRequest) -> HttpResponseBase:
-        before_session_data = dict(request.session.items()) if config.DEBUG_COLLECT_SESSION_DATA else {}
         response = self.get_response(request)
-        after_session_data = dict(request.session.items()) if config.DEBUG_COLLECT_SESSION_DATA else {}
 
         logger_extra = LoggerExtraType(
             aws_request_id=get_aws_request_id_from_request(request),
             data=LoggerExtraDataType(request=get_request_log_data(request), response=get_response_log_data(response)),
         )
-        if config.DEBUG_COLLECT_SESSION_DATA:
-            logger_extra["session"] = {"before": before_session_data, "after": after_session_data}
 
         cloudwatch_logger.info(msg="log_request", extra=logger_extra)
 
