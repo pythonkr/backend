bd507ade72e36c36bc7b639f4c88b81e342fa94d
MUsoftware <musoftware@daum.net>
2025-06-08 17:57:47 +0900

    feat: UserExt 모델에 nickname 추가 및 Organization.name을 비어있지 않게 수정

--- app/user/translation.py
+++ app/user/translation.py
@@ -1,5 +1,11 @@
 from modeltranslation.translator import TranslationOptions, register
 from user.models.organization import Organization
+from user.models.user import UserExt
+
+
+@register(UserExt)
+class UserExtTranslationOptions(TranslationOptions):
+    fields = ("nickname",)
 
 
 @register(Organization)
