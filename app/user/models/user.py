882140bd1fd09d9e5d247875673da7b5d3c09a86
MUsoftware <musoftware@daum.net>
2025-06-08 23:08:14 +0900

    fix: django-simple-history 문제로 인해 unique 제한 조건을 변경

--- app/user/models/user.py
+++ app/user/models/user.py
@@ -3,10 +3,20 @@ from django.db import models
 
 
 class UserExt(AbstractUser):
-    nickname = models.CharField(max_length=128, unique=True, null=True, blank=True)
+    nickname = models.CharField(max_length=128, null=True, blank=True)
 
     class Meta:
         ordering = ["-date_joined"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["nickname"],
+                name="uq__userext__nickname",
+                condition=models.Q(
+                    nickname__isnull=False,
+                    is_active=True,
+                ),
+            ),
+        ]
 
     def __str__(self):
         return f"{self.nickname} <{self.email}>"
