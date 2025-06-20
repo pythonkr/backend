db03a5c24977b4a39a0d6b3de10de1304d3d764b
MUsoftware <musoftware@daum.net>
2025-06-20 17:05:58 +0900

    feat: SponsorTag 추가

--- app/event/sponsor/migrations/0007_sponsor_sponsor_tiers.py
+++ app/event/sponsor/migrations/0007_sponsor_sponsor_tiers.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name="sponsor",
-            name="sponsor_tiers",
+            name="tiers",
             field=models.ManyToManyField(through="sponsor.SponsorTierSponsorRelation", to="sponsor.sponsortier"),
         ),
     ]
--- app/event/sponsor/serializers.py
+++ app/event/sponsor/serializers.py
@@ -4,10 +4,11 @@ from rest_framework import serializers
 
 class SponsorSerializer(serializers.ModelSerializer):
     logo = serializers.FileField(source="logo.file", read_only=True)
+    tags = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Sponsor
-        fields = ("id", "name", "logo", "sitemap_id")
+        fields = ("id", "name", "logo", "sitemap_id", "tags")
 
 
 class SponsorTierSerializer(serializers.ModelSerializer):
