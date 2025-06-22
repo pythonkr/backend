423a2075d8c127dd7d937e2aa86b6aef920d77ca
MUsoftware <musoftware@daum.net>
2025-06-22 20:39:56 +0900

    fix: 세션 모델을 새 설계에 맞게 수정

--- app/event/presentation/models.py
+++ app/event/presentation/models.py
@@ -1,13 +1,10 @@
 from __future__ import annotations
 
-import collections.abc
-import contextlib
-import functools
-
 from core.models import BaseAbstractModel, BaseAbstractModelQuerySet, MarkdownField
 from django.contrib.auth import get_user_model
 from django.db import models
 from event.models import Event
+from file.models import PublicFile
 
 User = get_user_model()
 
@@ -65,28 +62,14 @@ class PresentationCategory(BaseAbstractModel):
 class Presentation(BaseAbstractModel):
     type = models.ForeignKey(PresentationType, on_delete=models.PROTECT)
     title = models.CharField(max_length=256)
-    page = models.ForeignKey(to="cms.Page", on_delete=models.PROTECT, null=True, blank=True)
-
+    description = MarkdownField(blank=True, default="")
+    image = models.ForeignKey(PublicFile, on_delete=models.PROTECT, null=True, blank=True)
     categories = models.ManyToManyField(to="PresentationCategory", through="PresentationCategoryRelation")
 
     objects: PresentationQuerySet = PresentationQuerySet.as_manager()
 
     def __str__(self) -> str:
         return f"[{self.type.name}] {self.title}"
-
-    @functools.cached_property
-    def active_categories(self) -> collections.abc.Iterable[PresentationCategory]:
-        with contextlib.suppress(AttributeError):
-            return self._prefetched_active_categories
-
-        return self.categories.filter_active()
-
-    @functools.cached_property
-    def active_speakers(self) -> collections.abc.Iterable[PresentationSpeaker]:
-        with contextlib.suppress(AttributeError):
-            return self._prefetched_active_speakers
-
-        return self.speakers.filter_active().select_related("user")
 
 
 class PresentationCategoryRelation(models.Model):
@@ -97,4 +80,5 @@ class PresentationCategoryRelation(models.Model):
 class PresentationSpeaker(BaseAbstractModel):
     presentation = models.ForeignKey(Presentation, on_delete=models.PROTECT, related_name="speakers")
     user = models.ForeignKey(User, on_delete=models.PROTECT)
+    image = models.ForeignKey(PublicFile, on_delete=models.PROTECT, null=True, blank=True)
     biography = MarkdownField(blank=True, default="")
