ecc4a79d21b2889e93e8840498020642f3c2a9ec
MUsoftware <musoftware@daum.net>
2025-07-06 02:29:50 +0900

    fix: API 경로의 오타 수정

--- app/admin_api/views/modification_audit.py
+++ app/admin_api/views/modification_audit.py
@@ -8,7 +8,7 @@ from core.permissions import IsSuperUser
 from django.db import models
 from drf_spectacular import utils
 from participant_portal_api.models import ModificationAudit, ModificationAuditComment
-from rest_framework import decorators, mixins, request, response, status, viewsets
+from rest_framework import decorators, mixins, request, response, viewsets
 
 
 @utils.extend_schema_view(
@@ -26,22 +26,16 @@ class ModificationAuditAdminViewSet(mixins.ListModelMixin, mixins.RetrieveModelM
         .select_related("created_by", "updated_by", "deleted_by")
     )
 
-    @utils.extend_schema(
-        tags=[OpenAPITag.ADMIN_MODIFICATION_AUDIT],
-        responses={status.HTTP_200_OK: ModificationAuditApprovalAdminSerializer},
-    )
-    @decorators.action(detail=True, methods=["patch"], url_path="aprove")
-    def cancel_audit(self, request: request.Request, *args: tuple, **kwargs: dict) -> response.Response:
+    @utils.extend_schema(tags=[OpenAPITag.ADMIN_MODIFICATION_AUDIT])
+    @decorators.action(detail=True, methods=["patch"], url_path="approve")
+    def approve_audit(self, request: request.Request, *args: tuple, **kwargs: dict) -> response.Response:
         serializer = ModificationAuditApprovalAdminSerializer(self.get_object(), data=request.data)
         serializer.is_valid(raise_exception=True)
         instance = serializer.save()
 
         return response.Response(data=self.get_serializer(instance).data)
 
-    @utils.extend_schema(
-        tags=[OpenAPITag.ADMIN_MODIFICATION_AUDIT],
-        responses={status.HTTP_200_OK: ModificationAuditRejectionAdminSerializer},
-    )
+    @utils.extend_schema(tags=[OpenAPITag.ADMIN_MODIFICATION_AUDIT])
     @decorators.action(detail=True, methods=["patch"], url_path="reject")
     def reject_audit(self, request: request.Request, *args: tuple, **kwargs: dict) -> response.Response:
         serializer = ModificationAuditRejectionAdminSerializer(self.get_object(), data=request.data)
