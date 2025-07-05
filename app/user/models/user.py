c924b40e10126714b2c983f033e67d4f55dc63db
MUsoftware <musoftware@daum.net>
2025-07-06 02:56:59 +0900

    fix: 누락된 모델 정의 추가

--- app/user/models/user.py
+++ app/user/models/user.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class UserExt(AbstractUser):
-    # image = models.ForeignKey("file.PublicFile", on_delete=models.PROTECT, null=True, blank=True)
+    image = models.ForeignKey("file.PublicFile", on_delete=models.PROTECT, null=True, blank=True)
     nickname = models.CharField(max_length=128, null=True, blank=True)
 
     class Meta:
