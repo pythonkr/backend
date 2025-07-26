36d329a75e7ab244a2e55333d9851f829adf6083
MUsoftware <musoftware@daum.net>
2025-07-26 21:47:02 +0900

    fix: 닉네임에 unique constraint 제거

--- app/user/models/user.py
+++ app/user/models/user.py
@@ -8,16 +8,6 @@ class UserExt(AbstractUser):
 
     class Meta:
         ordering = ["-date_joined"]
-        constraints = [
-            models.UniqueConstraint(
-                fields=["nickname"],
-                name="uq__userext__nickname",
-                condition=models.Q(
-                    nickname__isnull=False,
-                    is_active=True,
-                ),
-            ),
-        ]
 
     def __str__(self):
         return f"[User] {self.nickname} <{self.email}>"
