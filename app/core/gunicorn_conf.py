d8fc5c3497d5880d5abc8b9114fbe8c5be06bfbf
MUsoftware <musoftware@daum.net>
2026-06-29 13:02:27 +0900

    feat: 메모리 release를 위한 설정 추가

--- app/core/gunicorn_conf.py
+++ app/core/gunicorn_conf.py
@@ -5,6 +5,8 @@ from core.observability import configure_opentelemetry
 bind = os.environ.get("GUNICORN_BIND", "0.0.0.0:8000")
 workers = int(os.environ.get("GUNICORN_WORKERS", "4"))
 timeout = int(os.environ.get("GUNICORN_TIMEOUT", "30"))
+max_requests = int(os.environ.get("GUNICORN_MAX_REQUESTS", "1000"))
+max_requests_jitter = int(os.environ.get("GUNICORN_MAX_REQUESTS_JITTER", "100"))
 
 
 def post_fork(server, worker):
